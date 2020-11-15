783a107522ccd51e3a0a4a3589f15757a7bf3309
Benjamin Sjølander <>
2020-11-15 13:52:36 +0100

    Add TychoLocationsSeeder

---
+++ database/seeders/TychoLocationsSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TychoLocationsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $locations = [];
+
+        $row = 0;
+        if (($handle = fopen("database/tycho.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                if ($row++ == 0)
+                    continue;
+
+                if (in_array([$data[5], $data[6], $data[7], $data[8], $data[9], $data[10]], $locations))
+                    continue;
+
+                $locations[] = [$data[5], $data[6], $data[7], $data[8], $data[9], $data[10]];
+
+                echo "Adding location '$data[5]', '$data[6]', '$data[7]', '$data[8]', '$data[9]', '$data[10]'\n";
+
+                DB::insert("INSERT INTO odb.locations
+                                  (CountryName, CountryIso, StateName, StateIso, CountyName, CityName)
+                                  VALUES ('$data[5]', '$data[6]', '$data[7]', '$data[8]', '$data[9]', '$data[10]')");
+
+                echo "Added location " . $data[10] . "\n";
+            }
+            fclose($handle);
+        }
+    }
+}

6021a7c1b1f0cae2559ec673418c3e92849616c1
Benjamin Sjølander <>
2020-11-15 12:18:51 +0100

    Add ODB creation script

---
+++ database/create_odb.sql
@@ -0,0 +1,42 @@
+drop schema odb;
+create schema odb;
+use odb;
+
+create table diseases (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    ConditionSNOMED INT,
+    ConditionName VARCHAR (25),
+    PathogenName VARCHAR (25),
+    PathogenTaxonId INT,
+    Fatalities INT
+);
+
+create table locations (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    CountryName VARCHAR (100),
+    CountryIso VARCHAR (2),
+    StateName VARCHAR (25),
+    StateIso VARCHAR (5),
+    CountyName VARCHAR (25),
+    CityName VARCHAR (25)
+);
+
+create table sources (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    SourceName VARCHAR (100)
+);
+
+create table cases (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    PeriodStart DATE,
+    PeriodEnd DATE,
+    DiseaseId INT NOT NULL,
+    LocationId INT NOT NULL,
+    SourceId INT NOT NULL,
+    Fatalities BOOL,
+    CountValue INT,
+    FOREIGN KEY (DiseaseId) references diseases (Id),
+    FOREIGN KEY (LocationId) references locations (Id),
+    FOREIGN KEY (SourceId) references sources (Id)
+);
+
